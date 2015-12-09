4f21c19617e49c92013599be92ef39b8e73cc1d0
laurenfazah <laurenfazah@gmail.com>
2015-12-09 15:03:50 -0500

    picks random chainsaw

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #glowing-chainsaw
 
-##[This tweet](https://twitter.com/OldManKris/status/673184195485790208) presented the game of creating a randomly named github repository and creating something based off of that name. I present you with "glowing-chainsaw"
+[This tweet](https://twitter.com/OldManKris/status/673184195485790208) presented the game of creating a randomly named github repository and creating something based off of that name. I present you with "glowing-chainsaw".
--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,27 @@
 $(function(){
     $chainsaw = $('svg.chainsaw-icon');
 
-    var count = 0;
+    var count = 0,
+        chainsaws = [$('.chainsaw.one'), $('.chainsaw.two'), $('.chainsaw.three')],
+        pickChainsaw = function(){
+            var sawIndex = function getRandomInt(min, max) {
+                return Math.floor(Math.random() * (max - min)) + min;
+            };
+            chainsaws[sawIndex(0,3)].css("display", "block");
+        },
+        glow = function(){
+            if (count%2===0){
+                $chainsaw.attr("class","chainsaw-icon");
+            } else {
+                $chainsaw.attr("class","chainsaw-icon glowing");
+            }
+            count++;
+        };
+
+    pickChainsaw();
+    glow();
 
     setInterval(function(){
-        if (count%2===0){
-            $chainsaw.attr("class","chainsaw-icon");
-        } else {
-            $chainsaw.attr("class","chainsaw-icon glowing");
-        }
-        count++;
+        glow();
     }, 1500);
 });
